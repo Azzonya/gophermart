a1f00b2308aaa420e44555d395b9df8c48286ff2
Azzonya <azamat.beisembinov@gmail.com>
2024-02-07 11:21:26 +0600

    e2e - auth fix

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -26,7 +26,7 @@ func (u *UserHandlers) RegisterUser(c *gin.Context) {
 
 	exist, err := u.userUsecase.IsLoginTaken(ctx, req.Login)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
+		c.JSON(http.StatusPreconditionRequired, gin.H{
 			"message": "Failed to check login",
 			"error":   err.Error(),
 		})
